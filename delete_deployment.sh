0ccec42f34129d915d51fadbdb66fa09d4ca4323
Vincent Emonet <vincent.emonet@gmail.com>
2020-03-25 13:09:39 +0100

    schuffle order of delete deployments in script

--- delete_deployment.sh
+++ delete_deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-oc delete -f flink-configuration-configmap.yaml
-oc delete -f jobmanager-service.yaml
-oc delete -f jobmanager-deployment.yaml
-oc delete -f taskmanager-deployment.yaml
 oc delete -f jobmanager-rest-service.yaml
+oc delete -f taskmanager-deployment.yaml
+oc delete -f jobmanager-deployment.yaml
+oc delete -f jobmanager-service.yaml
+oc delete -f flink-configuration-configmap.yaml
